81228fc8f638cb8b1502948b2f49c604121f48db
TanzidAnan <tanzid.anan125@gmail.com>
2025-11-10 23:05:27 +0600

    text animation

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
             <div className="hero">
                 <div className="hero-content text-center">
                     <div className="max-w-lg">
-                        <h1 className="text-5xl font-bold">Welcome to <span>byteBlaze</span> </h1>
+                        <h1 className="text-5xl font-bold">Welcome to <span className='text-5xl font-bold bg-gradient-to-r from-orange-700 via-blue-500 to-green-400 text-transparent bg-clip-text animate-gradient'>ByteBlaze</span> </h1>
                         <p className="py-6">
                             ByteBlaze is the bridge between the complex world of thechnology and the curious minds eager to understand it.
                         </p>
