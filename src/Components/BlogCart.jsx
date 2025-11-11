ec49b67a78c8d3ed77484110f7bbea305eece31b
TanzidAnan <tanzid.anan125@gmail.com>
2025-11-12 00:07:50 +0600

    blog page

--- src/Components/BlogCart.jsx
+++ src/Components/BlogCart.jsx
@@ -6,7 +6,7 @@ const BlogCart = ({blog}) => {
     const {cover_image,title,description,published_at,id} =blog;
     return (
         <div>
-            <Link to={`/blog/${id}`} rel="noopener noreferrer" className="max-w-sm transition border-2 hover:scale-105 border-amber-200  mx-auto group hover:no-underline focus:no-underline dark:bg-gray-50 hidden sm:block">
+            <Link to={`/blog/${id}`} rel="noopener noreferrer" className="max-w-sm transition border-opacity-30 border-2 hover:scale-105 border-amber-200  mx-auto group hover:no-underline focus:no-underline dark:bg-gray-50 hidden sm:block">
                             <img role="presentation" className="object-cover w-full rounded h-44 dark:bg-gray-500" src={cover_image ||placeholderImage} />
                             <div className="p-6 space-y-2">
                                 <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">{title}</h3>
