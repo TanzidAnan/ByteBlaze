892355f7095a6ecadfcdfe5715d2ab2bd9ccdfd9
TanzidAnan <tanzid.anan125@gmail.com>
2025-11-11 23:32:16 +0600

    blog

--- src/Components/BlogCart.jsx
+++ src/Components/BlogCart.jsx
@@ -6,12 +6,12 @@ const BlogCart = ({blog}) => {
     const {cover_image,title,description,published_at,id} =blog;
     return (
         <div>
-            <Link to={`/blog/${id}`} rel="noopener noreferrer" href="#" className="max-w-sm mx-auto group hover:no-underline focus:no-underline dark:bg-gray-50 hidden sm:block">
+            <Link to={`/blog/${id}`} rel="noopener noreferrer" className="max-w-sm transition border-2 hover:scale-105 border-amber-200  mx-auto group hover:no-underline focus:no-underline dark:bg-gray-50 hidden sm:block">
                             <img role="presentation" className="object-cover w-full rounded h-44 dark:bg-gray-500" src={cover_image ||placeholderImage} />
                             <div className="p-6 space-y-2">
-                                <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">In usu laoreet repudiare legendos</h3>
-                                <span className="text-xs dark:text-gray-600">January 26, 2021</span>
-                                <p>Mei ex aliquid eleifend forensibus, quo ad dicta apeirian neglegentur, ex has tantas percipit perfecto. At per tempor albucius perfecto, ei probatus consulatu patrioque mea, ei vocent delicata indoctum pri.</p>
+                                <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">{title}</h3>
+                                <span className="text-xs dark:text-gray-600">{new Date(published_at).toLocaleDateString()}</span>
+                                <p>{description}</p>
                             </div>
                         </Link>
         </div>
